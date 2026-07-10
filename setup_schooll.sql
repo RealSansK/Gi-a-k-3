0918ae61e3a6ba53581ca4aed18a815b4f1578c0
RealSansK <151824465+RealSansK@users.noreply.github.com>
2026-07-10 23:16:28 +0700

    Update setup_schooll.sql

--- setup_schooll.sql
+++ setup_schooll.sql
@@ -2,11 +2,11 @@
 CREATE DATABASE IF NOT EXISTS schooll;
 USE schooll;
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS records (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,   -- luu SHA-256 hash
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    strname VARCHAR(100) NOT NULL,
+    course VARCHAR(100) NOT NULL,
+    fee DECIMAL(10,2) NOT NULL
 );
 
 INSERT INTO users (username, password)
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS records (
     fee DECIMAL(10,2) NOT NULL
 );
 
-INSERT INTO records (name, course, fee) VALUES
+INSERT INTO records (strname, course, fee) VALUES
 ('Nguyen Van A', 'Java Web', 1500000),
 ('Tran Thi B', 'Database', 1200000),
 ('Le Van C', 'Network', 1000000);
